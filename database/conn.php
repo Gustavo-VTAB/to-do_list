cd65a7d8668f261a3d419da7614a372390f3accf
Gustavo Novais <gust.inbug@gmail.com>
2025-04-04 16:56:38 -0300

    port change again

--- database/conn.php
+++ database/conn.php
@@ -1,14 +1,14 @@
 <?php
     
     $hostname = 'localhost';
-    $port = '9000';
+    //$port = '9000';
     $database = 'to_do_list';
     $username = 'root';
     $password = 'admin';
 
     //criando a conexão com o banco de dados
     try {
-        $pdo = new PDO("mysql:host=$hostname;port=$port; dbname=$database", $username, $password); // Criação do objeto PDO
+        $pdo = new PDO("mysql:host=$hostname; dbname=$database", $username, $password); // Criação do objeto PDO
         
     } catch (PDOException $e) {
         echo "Connection failed: " . $e->getMessage(); // Caso ocorra algum erro, exibe a mensagem de erro
