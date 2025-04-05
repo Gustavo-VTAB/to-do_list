d25f3e7658989053a7789942526ba81dd8bb74eb
Gustavo Novais <gust.inbug@gmail.com>
2025-04-04 21:51:23 -0300

    remove configuração de exibição de erros do PHP no arquivo de criação de usuário

--- actions-for-account/create.php
+++ actions-for-account/create.php
@@ -1,13 +1,6 @@
-
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-
-header('Content-Type: application/json');
-
 session_start();
+header('Content-Type: application/json');
 require_once('../database/conn.php'); // Caminho local para o seu banco
 
 
